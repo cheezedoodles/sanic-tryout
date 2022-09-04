38680e462b545db48ee16c609cbf60cb61bda426
cheezedoodles <hougvapes@gmail.com>
2022-09-04 22:49:49 +0300

    endpoint for retrieving goods

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 from sanic import Sanic, text
+from sanic.response import json
+from sanic.exceptions import SanicException
 
 from sanic_jwt import initialize, protected
 
 from tortoise.contrib.sanic import register_tortoise
 
 from auth import registration, authenticate
+from models import Products
+from utils import ROWS_PER_PAGE
 
 
 app = Sanic(__name__)
@@ -18,6 +22,25 @@ app.blueprint(registration)
 @protected()
 async def hello_world(request):
     return text('welcome')
+
+
+@app.get("/goods/page/<page:int>")
+async def get_goods(request, page):
+    """
+    returns a list of goods (in quantity of ROWS_PER_PAGE)
+    """
+    if page <= 0:
+        raise SanicException("Invalid page number", status_code=400)
+    limit = ROWS_PER_PAGE * page
+    offset = ROWS_PER_PAGE * (page - 1)
+    # next = page + 1
+    products = await Products.all().offset(offset=offset)\
+                                   .limit(limit=limit)\
+                                   .values()
+    # if len(products) < ROWS_PER_PAGE:
+    #     next = None
+    # products.append({'next': next})
+    return json(products)
 
 
 register_tortoise(
--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
+ROWS_PER_PAGE = 5
+
+
 async def make_verification_link(id, username):
+    """
+    creates a link for account activation
+    """
     link = str(id)+username
     return link
 
 
 async def verify_link(link, id, username):
+    """
+    verifies that the link is valid
+    """
     if link == str(id)+username:
         return True
     return False
